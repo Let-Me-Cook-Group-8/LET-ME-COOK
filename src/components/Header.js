16d120b15e203404105e3ecfdd94f5b87fb723ab
pthao207203 <149165346+pthao207203@users.noreply.github.com>
2024-05-31 21:27:43 +0700

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/Header.css';
 import Letmecook from '../image/Letmecook.png';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { click } from '@testing-library/user-event/dist/click';
 
-export default function Header() {
-    return (
+//export default function Header() {
+class Header extends Component {
+    state = { clicked: false };
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked });
+    }
+    render() {
+        return (
 
-        <header>
-            <nav class="header navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-                <div class="container-fluid">
-                    <a class="navbar-branch" href="#home" id="logo">
-                        <img src={Letmecook} style={{ height: '50px' }} alt="Logo" />
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
-                        <span class="nsavbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                        <ul class="ms-auto navbar-nav">
-                            <li class="nav-item">
-                                <Link className="nav-link" to="/">Trang Chủ</Link>
-                            </li>
-                            <li class="nav-item">
-                                <a className="nav-link" href="#about">Giới thiệu</a>
-                            </li>
-                            <li class="nav-item">
-                                <a className="nav-link" href="#main-content">Nổi bật</a>
-                            </li>
-                            <li class="nav-item">
-                                <Link className="nav-link" to="/search">Tìm kiếm</Link>
-                            </li>
-                            <li class="nav-item">
-                                <a className="nav-link" href="#contact">Liên hệ</a>
-                            </li>
-                            <li class="nav-item">
-                                <Link className="nav-link" to="/login">Đăng nhập</Link>
-                            </li>
-                            <li class="nav-item">
-                                <Link className="nav-link" to="/Addblog">Viết blog</Link>
-                            </li>
-                        </ul>
+            <header class="sticky-top" onClick={this.handleClick}>
+                <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div class="container-fluid">
+                        <a class="navbar-branch" href="#logo" id="logo">
+                            <img src={Letmecook} style={{ height: '50px' }} alt="Logo" />
+                        </a>
+                        <div class="collapse navbar-collapse" id="navbarResponsive" className={this.state.clicked ? "#navbarResponsive active" : "#navbarResponsive"}>
+                            <ul class="navbar-nav ms-auto">
+                                <li class="nav-item">
+                                    <Link className="nav-link" to="/">Trang Chủ</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <a className="nav-link" href="#about">Giới thiệu</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a className="nav-link" href="#main-content">Nổi bật</a>
+                                </li>
+                                <li class="nav-item">
+                                    <Link className="nav-link" to="/search">Tìm kiếm</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <a className="nav-link" href="#contact">Liên hệ</a>
+                                </li>
+                                <li class="nav-item">
+                                    <Link className="nav-link" to="/login">Đăng nhập</Link>
+                                </li>
+                                <li class="nav-item">
+                                    <Link className="nav-link" to="/Addblog">Viết blog</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div id="mobile" onClick={this.handleClick}>
+                            <i id="bar" className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </header>
+                </nav>
+            </header>
 
-    );
+        );
+    }
 }
+export default Header;
