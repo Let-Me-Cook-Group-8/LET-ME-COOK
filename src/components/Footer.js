d6b56bcfedbf62f417633edae04d32d1ea4bdcbf
pthao207203 <149165346+pthao207203@users.noreply.github.com>
2024-05-31 18:43:48 +0700

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,52 @@
 import React from 'react'
 import '../css/Footer.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Footer() {
     return (
         <footer id="contact">
             <div className="footer-main">
-                <div className="footer-tag">
-                    <h2 className="footer-title">Top Food</h2>
-                    <p className="footer-discri">Bún bò</p>
-                    <p className="footer-discri">Bò kho</p>
-                    <p className="footer-discri">Trứng chiên</p>
-                    <p className="footer-discri">Giả cầy</p>
-                </div>
-
-                <div className="footer-tag">
-                    <h2 className="footer-title">Liên kết</h2>
-                    <p className="footer-discri">Trang chủ</p>
-                    <p className="footer-discri">Giới thiệu</p>
-                    <p className="footer-discri">Menu</p>
-                    <p className="footer-discri">Tìm kiếm</p>
-                    <p className="footer-discri">Blog</p>
-                </div>
-                <div className="footer-tag">
-                    <h2 className="footer-title">Liên hệ</h2>
-                    <p className="footer-discri">01237456788</p>
-                    <p className="footer-discri">03456234567</p>
-                    <p className="footer-discri">letmecook1122@gmail.com</p>
-                    <p className="footer-discri">webnhom8123@gmail.com</p>
-                </div>
-                <div className="footer-tag">
-                    <h2 className="footer-title">Theo dõi</h2>
-                    <i className="fa-brands fa-square-facebook"></i>
-                    <i className="fa-brands fa-square-instagram"></i>
-                    <i className="fa-brands fa-square-twitter"></i>
-                    <i className="fa-brands fa-youtube"></i>
-                </div>
+                <Row>
+                    <Col lg={3} xs={6}>
+                        <div className="footer-tag">
+                            <h2 className="footer-title">Top Food</h2>
+                            <p className="footer-discri">Bún bò</p>
+                            <p className="footer-discri">Bò kho</p>
+                            <p className="footer-discri">Trứng chiên</p>
+                            <p className="footer-discri">Giả cầy</p>
+                        </div>
+                    </Col>
+                    <Col lg={3} xs={6}>
+                        <div className="footer-tag">
+                            <h2 className="footer-title">Liên kết</h2>
+                            <p className="footer-discri">Trang chủ</p>
+                            <p className="footer-discri">Giới thiệu</p>
+                            <p className="footer-discri">Menu</p>
+                            <p className="footer-discri">Tìm kiếm</p>
+                            <p className="footer-discri">Blog</p>
+                        </div>
+                    </Col>
+                    <Col lg={3} xs={6}>
+                        <div className="footer-tag">
+                            <h2 className="footer-title">Liên hệ</h2>
+                            <p className="footer-discri">01237456788</p>
+                            <p className="footer-discri">03456234567</p>
+                            <p className="footer-discri">letmecook1122@gmail.com</p>
+                            <p className="footer-discri">webnhom8123@gmail.com</p>
+                        </div>
+                    </Col>
+                    <Col lg={3} xs={6}>
+                        <div className="footer-tag">
+                            <h2 className="footer-title">Theo dõi</h2>
+                            <i className="fa-brands fa-square-facebook"></i>
+                            <i className="fa-brands fa-square-instagram"></i>
+                            <i className="fa-brands fa-square-twitter"></i>
+                            <i className="fa-brands fa-youtube"></i>
+                        </div>
+                    </Col>
+                </Row>
             </div>
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,48 @@ import React from 'react';
 import '../css/Header.css';
 import Letmecook from '../image/Letmecook.png';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Header() {
     return (
+
         <header>
-            <div id="header">
-                <div id="top-left">
-                    <a href="" id="logo" >
-                        <img src={Letmecook} style={{ height: '7vh' }} alt="Logo" />
+            <nav class="header navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+                <div class="container-fluid">
+                    <a class="navbar-branch" href="#home" id="logo">
+                        <img src={Letmecook} style={{ height: '50px' }} alt="Logo" />
                     </a>
-                </div>
-
-                <div id="top-right">
-                    <div id="nav">
-                        <Link className="nav-link" to="/">Trang Chủ</Link>
-                        <a className="nav-link" href="#about">Giới thiệu</a>
-
-                        <a className="nav-link" href="#main-content">Nổi bật</a>
-                        <Link className="nav-link" to="/search">Tìm kiếm</Link>
-                        <a className="nav-link" href="#contact">Liên hệ</a>
-                        <Link className="nav-link" to="/login">Đăng nhập</Link>
-                        <Link className="nav-link" to="/Addblog">Viết blog</Link>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                        <span class="nsavbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="ms-auto navbar-nav">
+                            <li class="nav-item">
+                                <Link className="nav-link" to="/">Trang Chủ</Link>
+                            </li>
+                            <li class="nav-item">
+                                <a className="nav-link" href="#about">Giới thiệu</a>
+                            </li>
+                            <li class="nav-item">
+                                <a className="nav-link" href="#main-content">Nổi bật</a>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-link" to="/search">Tìm kiếm</Link>
+                            </li>
+                            <li class="nav-item">
+                                <a className="nav-link" href="#contact">Liên hệ</a>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-link" to="/login">Đăng nhập</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-link" to="/Addblog">Viết blog</Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </div>
+            </nav>
         </header>
+
     );
 }
