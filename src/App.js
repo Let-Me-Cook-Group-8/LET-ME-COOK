ec62729e640ef5675822391309800772c518ab11
Hieu <22520442@gm.uit.edu.vn>
2024-05-31 15:45:02 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,18 +3,13 @@ import React from 'react';
 import Footer from '../src/components/Footer'
 import { Outlet } from 'react-router-dom';
 import CardFood from '../src/components/CardFood.js'
-import  { useState } from 'react';
-export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLoginSuccess = () => {
-      setIsLoggedIn(true);
-  }
+function App() {
   return (
     <div className='App'>
       <div className='App'>
         <div className='Header'>
-        <Header isLoggedIn={isLoggedIn} />
+          <Header />
         </div>
         <Outlet />
 
@@ -26,4 +21,4 @@ export default function App() {
 
 }
 
-
+export default App;
